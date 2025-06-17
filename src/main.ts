960b5afc3cc1f93b382b0e5b56d950f8cd05fddf
exwhyeye <Vaas820@yandex.ru>
2025-06-17 12:17:57 +0700

    Fixed main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import { createApp } from 'vue'
 import { createVfm } from 'vue-final-modal'
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
-
-app.use(router)
 app.use(createVfm())
 app.mount('#app')
